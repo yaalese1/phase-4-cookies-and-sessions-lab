93e30a9b1f333d4cf566d495d63a553d85239c60
Yetunde Alese <yaalese1@gmail.com>
2023-01-20 12:46:15 -0500

    commit tree b5cf30d086672ed817976d069e694f7529e0d2ad

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,15 +6,32 @@ class ArticlesController < ApplicationController
     render json: articles, each_serializer: ArticleListSerializer
   end
 
-  def show
-    article = Article.find(params[:id])
-    render json: article
-  end
+  # def show
+  #   article = Article.find(params[:id])
+  #   render json: article
+    
+  # end
+
+  def page_view
+    session[:page_views] ||=0
+    session [:page_views] +=1
+    if 
+     session[:page_views] <=3
+      article = Article.find_by(id: params[:id])
+      render json: artice 
+    else
+
+   maximum_views
+    end
+
+  end 
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
+ def maximum_views
+  render json: { error: "you have reached your maxed views, please subscribe to continue"}, status:  503 
+ end
 end
